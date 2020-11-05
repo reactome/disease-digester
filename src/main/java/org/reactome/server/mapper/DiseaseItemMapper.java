0bcad9a5fb43358fdc548bc1632e1061ce90d99e
chuan deng <dengchuanbio@gmail.com>
2020-11-05 11:19:09 +0800

    replace '$' with '#' in mybtais query

--- src/main/java/org/reactome/server/mapper/DiseaseItemMapper.java
+++ src/main/java/org/reactome/server/mapper/DiseaseItemMapper.java
@@ -18,22 +18,22 @@ public interface DiseaseItemMapper {
     @Select("select count(d.diseaseId) from disease d inner join gene g on d.diseaseId = g.diseaseId where score >= ${score} group by d.diseaseId having count(g.geneId) >= ${size}")
     List<Integer> selectDiseaseIdCount(@Param("score") Float score, @Param("size") Integer geneSize);
 
-    @Select("select d.diseaseId from disease d inner join gene g on d.diseaseId = g.diseaseId where diseaseName like concat('%','${diseaseName}','%') and score >= ${score} group by d.diseaseId having count(g.geneId) >= ${size} order by count(g.geneId) ${order}, d.diseaseName ${order} limit ${pageSize} offset ${offset}")
+    @Select("select d.diseaseId from disease d inner join gene g on d.diseaseId = g.diseaseId where diseaseName like concat('%', #{diseaseName} ,'%') and score >= ${score} group by d.diseaseId having count(g.geneId) >= ${size} order by count(g.geneId) ${order}, d.diseaseName ${order} limit ${pageSize} offset ${offset}")
     List<String> selectDiseaseIdByDiseaseName(@Param("diseaseName") String diseaseName, @Param("score") Float score, @Param("size") Integer geneSize, @Param("order") String order, @Param("pageSize") Integer pageSize, @Param("offset") Integer offset);
 
-    @Select("select count(d.diseaseId) from disease d inner join gene g on d.diseaseId = g.diseaseId where diseaseName like concat('%','${diseaseName}','%') and score >= ${score} group by d.diseaseId having count(g.geneId) >= ${size}")
+    @Select("select count(d.diseaseId) from disease d inner join gene g on d.diseaseId = g.diseaseId where diseaseName like concat('%', #{diseaseName} ,'%') and score >= ${score} group by d.diseaseId having count(g.geneId) >= ${size}")
     List<Integer> selectDiseaseIdByDiseaseNameCount(@Param("diseaseName") String diseaseName, @Param("score") Float score, @Param("size") Integer geneSize);
 
     @Select("select d.diseaseId from disease d inner join gene g on d.diseaseId = g.diseaseId where score >= ${score} group by d.diseaseId having count(g.geneId) >= ${size} order by d.diseaseName ${order} limit ${pageSize} offset ${offset}")
     List<String> selectDiseaseIdOrderByDiseaseName(@Param("score") Float score, @Param("size") Integer geneSize, @Param("order") String order, @Param("pageSize") Integer pageSize, @Param("offset") Integer offset);
 
-    @Select("select d.diseaseId from disease d inner join gene g on d.diseaseId = g.diseaseId where diseaseName like concat('%','${diseaseName}','%') and score >= ${score} group by d.diseaseId having count(g.geneId) >= ${size} order by d.diseaseName ${order} limit ${pageSize} offset ${offset}")
+    @Select("select d.diseaseId from disease d inner join gene g on d.diseaseId = g.diseaseId where diseaseName like concat('%', #{diseaseName} ,'%') and score >= ${score} group by d.diseaseId having count(g.geneId) >= ${size} order by d.diseaseName ${order} limit ${pageSize} offset ${offset}")
     List<String> selectDiseaseIdByDiseaseNameAndOrderByDiseaseName(@Param("diseaseName") String diseaseName, @Param("score") Float score, @Param("size") Integer geneSize, @Param("order") String order, @Param("pageSize") Integer pageSize, @Param("offset") Integer offset);
 
     @Select("select max(c) from (select count(g.geneId) c from disease d inner join gene g on d.diseaseId = g.diseaseId where score >= ${score} group by d.diseaseId) _")
     Integer selectMaxGeneSizeByScore(@Param("score") Float score);
 
-    @Select("select max(c) from (select count(g.geneId) c from disease d inner join gene g on d.diseaseId = g.diseaseId where diseaseName like concat('%','${diseaseName}','%') and score >= ${score} group by d.diseaseId) _")
+    @Select("select max(c) from (select count(g.geneId) c from disease d inner join gene g on d.diseaseId = g.diseaseId where diseaseName like concat('%', #{diseaseName},'%') and score >= ${score} group by d.diseaseId) _")
     Integer selectMaxGeneSizeByScoreAndDiseaseName(@Param("diseaseName") String diseaseName, @Param("score") Float score);
 
     @Select("<script> " +
@@ -47,6 +47,6 @@ public interface DiseaseItemMapper {
     @Select("select diseaseName from disease")
     List<String> selectDiseaseName();
 
-    @Select("select geneSymbol from gene where diseaseId = '${diseaseId}'")
+    @Select("select geneSymbol from gene where diseaseId = #{diseaseId}")
     List<String> getGeneListByDiseaseId(@Param("diseaseId") String diseaseId);
 }
